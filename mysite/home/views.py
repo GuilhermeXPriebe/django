e3273c1373fc2b0dce0020405f6e0809940831b1
GuilhermePriebe <guilhermepriebe99@gmail.com>
2022-02-23 12:01:03 -0300

    My first Hello world passing arguments to the main page

---
+++ mysite/home/views.py
@@ -0,0 +1,13 @@
+from operator import index
+from re import TEMPLATE
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "TEMPLATES"),'
+)
+
+def index(request):
+    today = datetime.datetime.now().date()
+    return render(request, "index.html", {"today": today})
